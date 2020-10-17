b5cdc7ac1bcc34a85fa5cccf09441879da0082f1
Rudis Sprenne <70488838+rsprenne@users.noreply.github.com>
2020-10-17 17:46:39 -0400

    Uploaded files for basic push notification program

---
+++ App.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class App extends Application{
+    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannels();
+    }
+
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "Channel 1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription("Channel 1 High Priority Test");
+
+            NotificationChannel channel2 = new NotificationChannel(
+                    CHANNEL_2_ID,
+                    "Channel 1",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel2.setDescription("Channel 2 Low Priority Test");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
+        }
+    }
+}
